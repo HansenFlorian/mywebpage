c493270eda9e1809e3c1998183e7f97cae1c53c8
HansenFlorian <moonlicht@hotmail.de>
2017-11-29 04:20:17 +0000

    Portfolio hand in.

---
+++ add_html_research.R
@@ -0,0 +1,24 @@
+#find
+
+.tocify .list-group-item {
+  border-radius: 0px;
+}
+
+#replace with
+
+.tocify .list-group-item {
+  border-radius: 0px;
+  background-color: hsl(122.5, 36.4%, 8%)
+}
+
+.list-group-item.active, .list-group-item.active:focus, .list-group-item.active:hover {
+  background-color: hsl(175, 100%, 25%);
+}
+
+.list-group-item.active, .list-group-item.active:focus {
+  background-color: hsl(122.5, 36.4%, 12.9%);
+}
+
+.list-style-type {
+  background-color: blue;
+}
